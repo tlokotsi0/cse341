e271aae88a58453c32e93d9b708df51e67684464
tlokotsi0 <amantusf@gmail.com>
2026-05-17 17:22:53 +0200

    helee

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -63,14 +63,23 @@ const updateUser = async (req, res) => {
 };
 
 const deleteUser = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db('Project1').collection('users').deleteOne({ _id: userId });
-  if (response.deletedCount > 0) {
-    res.status(204).send(); 
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  try {
+    const userId = new ObjectId(req.params.id);
+
+    const response = await mongodb
+      .getDb()
+      .db('Project1')
+      .collection('users')
+      .deleteOne({ _id: userId });
+
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'User not found.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  
 };
 
 module.exports = {
