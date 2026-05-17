356e890d8b64eb2d16c758b41a6e331d0aabefb3
tlokotsi0 <amantusf@gmail.com>
2026-05-17 17:14:10 +0200

    deleteOne

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -64,7 +64,7 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db('Project1').collection('users').remove({ _id: userId }, true);
+  const response = await mongodb.getDb().db('Project1').collection('users').deleteOne({ _id: userId });
   if (response.deletedCount > 0) {
     res.status(204).send(); 
   } else {
